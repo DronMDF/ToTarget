5b280a8470e85b9300ad830e720c1fc465c70a28
Andrey Valyaev <dron.valyaev@gmail.com>
2015-05-06 16:58:50 +0300

    Удалось увидеть вложенные элементы, правда пришлось отказаться от кастомного виджета, и в моделере элементы не отображаются

--- app/src/main/java/com/mdf/totarget/MainActivity.java
+++ app/src/main/java/com/mdf/totarget/MainActivity.java
@@ -2,8 +2,12 @@ package com.mdf.totarget;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +16,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout layout = (LinearLayout)findViewById(R.id.tasks);
+        LayoutInflater inflater = getLayoutInflater();
+        for (int i = 0; i < 3; i++) {
+            View item = inflater.inflate(R.layout.task_view, layout, false);
+            EditText name = (EditText)item.findViewById(R.id.name);
+            name.setText(R.string.task_default);
+            layout.addView(item);
+        }
     }
 
     @Override
