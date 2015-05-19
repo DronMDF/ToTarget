f3bf23ac0a2c248bd1e4c5e2d55d33d4b175fb66
Andrey Valyaev <dron.valyaev@gmail.com>
2015-05-19 23:27:14 +0300

    Проблемы с тестами закончились... Тесты фейлятся по понятным причинам

--- app/src/androidTest/java/com/mdf/totarget/MainActivityTest.java
+++ app/src/androidTest/java/com/mdf/totarget/MainActivityTest.java
@@ -1,14 +1,16 @@
 package com.mdf.totarget;
 
-import android.content.Intent;
-import android.test.ActivityUnitTestCase;
+import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
+import static android.test.TouchUtils.*;
+import static android.test.ViewAsserts.*;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
     private MainActivity activity;
 
     public MainActivityTest() {
@@ -18,8 +20,7 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
     @Override
     public void setUp() throws Exception {
         super.setUp();
-        Intent intent = new Intent(getInstrumentation().getTargetContext(), MainActivity.class);
-        startActivity(intent, null, null);
+        setActivityInitialTouchMode(true);
         activity = (MainActivity) getActivity();
     }
 
@@ -33,6 +34,18 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
         getInstrumentation().waitForIdleSync();
         getInstrumentation().sendStringSync(text);
         getInstrumentation().waitForIdleSync();
+    }
+
+    @MediumTest
+    public void testTaskOnScreen() {
+        final View view = activity.getWindow().getDecorView();
+        for (int i = 0; i < 3; i++) {
+            LinearLayout tasks = (LinearLayout) activity.findViewById(R.id.tasks);
+            View task = tasks.getChildAt(i);
+            assertOnScreen(view, task.findViewById(R.id.name));
+            assertOnScreen(view, task.findViewById(R.id.spinner));
+            assertOnScreen(view, task.findViewById(R.id.done));
+        }
     }
 
     @MediumTest
@@ -59,8 +72,7 @@ public class MainActivityTest extends ActivityUnitTestCase<MainActivity> {
 
         View task_button = tasks.findViewById(R.id.done);
         // When
-        task_button.performClick();
-        getInstrumentation().waitForIdleSync();
+        clickView(this, task_button);
         // Then
         assertFalse(task_button.isEnabled());
         assertTrue(TextUtils.isEmpty(task_text.getText().toString()));
